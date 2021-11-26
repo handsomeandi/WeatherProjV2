91045afe2b2df3f592d85462402ca04a08e7ae8e
handsomeandi <gooroochanel3@gmail.com>
2021-11-26 15:42:01 +0300

    updated library versions and added fixed navigation logic

--- app/src/main/java/com/example/weatherv2/ui/WeatherApp.kt
+++ app/src/main/java/com/example/weatherv2/ui/WeatherApp.kt
@@ -3,15 +3,12 @@ package com.example.weatherv2.ui
 import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -56,9 +53,6 @@ fun WeatherApp() {
 
 @ExperimentalPermissionsApi
 fun NavGraphBuilder.navGraph(navController: NavController) {
-    //TODO: create shared weather viewModel that contains current town and current location
-//    val sharedViewMode: SharedViewModel = hiltViewModel<SharedViewModel>()
-
     composable("${MainDestinations.WEATHER_SCREEN}?${MainDestinations.TOWN_NAME}={townName}") { from ->
         val weatherViewModel = hiltViewModel<WeatherViewModel>()
         WeatherScreen(weatherViewModel, from.arguments?.getString(MainDestinations.TOWN_NAME))
@@ -67,7 +61,13 @@ fun NavGraphBuilder.navGraph(navController: NavController) {
     composable(MainDestinations.TOWNS_SCREEN) { from ->
         val townsViewModel = hiltViewModel<TownsViewModel>()
         TownsScreen(townsViewModel) {
-            navController.navigate("${MainDestinations.WEATHER_SCREEN}?${MainDestinations.TOWN_NAME}=$it")
+            val route = "${MainDestinations.WEATHER_SCREEN}?${MainDestinations.TOWN_NAME}=$it"
+            navController.navigate(route) {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    inclusive = true
+                }
+
+            }
         }
         Log.d("testing", "townScreenCalled")
     }
